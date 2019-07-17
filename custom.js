2ea9bb94dbbdd9f57adcfb3fdaa39571f3e5aafe
willburstein <52061430+willburstein@users.noreply.github.com>
2019-07-17 11:46:41 -0400

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,58 @@
+let queue = ["Claire", "Charlie", "Alex", "Adam", "Ben", "Caleb", "Jack", "Will", "Jarret"];
+let queueIndex = 0;
+let startTime = [30,0];
+let timeRemaining = (startTime[0] * 60 * 1000) + (startTime[1] * 1000);
+const writeable = document.getElementById("playing");
+const timer = document.getElementById("time");
+const audio = new Audio("alert.mp3");
+queue = prompt("Enter students (StudentA, StudentB, ...)", "Claire, Charlie, Alex, Adam, Ben, Caleb, Jack, Will, Jarret").split(", ");
+function setInner() {
+  writeable.innerHTML = "Student " + (queue[queueIndex]) + ", " + (queue[queueIndex+1]) + " are now playing";
+}
+
+document.getElementById("toggle").addEventListener("click", (e) => {
+  e.preventDefault();
+  const s = e.target;
+  if (s.innerHTML == "Start") {
+    audio.pause();
+    startCycle();
+    s.innerHTML = "Pause";
+  }
+  else {
+    audio.play();
+    clearInterval(i);
+    s.innerHTML = "Start";
+  }
+});
+
+function updateTimer() {
+  let mins=0, secs=0;
+  const raw = timeRemaining/60/1000;
+  const dec = ("" + raw).split(".");
+  if (dec.length == 2) {
+    mins = dec[0];
+    secs = parseInt(Math.round(parseFloat("." + dec[1]) * 60));
+  }
+  else {
+    mins = raw;
+  }
+  timer.innerHTML = mins + 'm ' + secs + 's';
+}
+timer.innerHTML = startTime[0] + "m " + startTime[1] + "s";
+setInner();
+let i;
+function startCycle() {
+    i = setInterval(()=>{
+    timeRemaining -= 1000;
+    if (timeRemaining == 0) {
+      queueIndex+=2;
+      audio.currentTime = 0;
+      audio.play();
+      clearInterval(i);
+      document.getElementById("toggle").innerHTML = "Start";
+      timeRemaining = (startTime[0] * 60 * 1000) + (startTime[1] * 1000);
+      setInner();
+    }
+    updateTimer();
+  }, 1000);
+}
